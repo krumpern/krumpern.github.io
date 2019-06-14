d9244c26597f27fcfeb8e9871c257fb0847c84a6
hannesklaus1 <hannesklaus@gmx.at>
2019-06-14 15:33:01 +0200

    POIs gelöscht welche keinen Namen Hatten, Statt SEEHÖHE null --> Seehöhe nicht vorhanden!

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ PointsofInterest.bindPopup(function (layer) {
   const NAME = (props.NAME)
   const popupText = `<h3>${props.NAME}</h3>
   <p>Seehöhe: ${props.SEEHOEHE}</p>
-  <p>Link: ${props.URL_INTERN}</p>`;
+  <p><footer> <a target = "blank", href="${props.URL_INTERN}"> Weitere Informationen</a><//footer>`;
   return popupText;
 });
 karte.addLayer(PointsofInterest);
